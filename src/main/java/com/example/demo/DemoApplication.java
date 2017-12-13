3041bd7ddf2852faa642456c53da998a863a1a06
Vivek Rana <vivekrana.2012@gmail.com>
2017-12-13 22:21:40 +0530

    Added Mongo Connection

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,22 @@
 package com.example.demo;
 
 import java.io.IOException;
+import java.util.Arrays;
 
+import org.bson.Document;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBCursor;
+import com.mongodb.MongoClient; 
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
@@ -22,14 +32,25 @@ public class DemoApplication {
 		
 		ObjectMapper mapper = new ObjectMapper();
 		String jsonString = "{\"status\":\"success\", \"userId\":\"vivek\"}";
-		
+	   
+	    // Creating Credentials 
+	    MongoCredential credential = MongoCredential.createCredential("app", "users", "app_password".toCharArray()); 
+	    
+	    // Creating a Mongo client 
+	    MongoClient mongo = new MongoClient( new ServerAddress("localhost" , 27017 ), Arrays.asList(credential)); 
+	     
+	    // Accessing the database 
+	    MongoDatabase database = mongo.getDatabase("users"); 
+	    MongoCollection<Document> collection = database.getCollection("authInfo");
+	    
+	    FindIterable<Document> cursor = collection.find(new BasicDBObject("username", "vivek"));
+	    
 		try {
-			JsonResponse response = mapper.readValue(jsonString, JsonResponse.class);
+			JsonResponse response = mapper.readValue(cursor.first().toString(), JsonResponse.class);
 			
 			return response;
 			
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
--- src/main/java/com/example/demo/JsonResponse.java
+++ src/main/java/com/example/demo/JsonResponse.java
@@ -1,24 +1,34 @@
 package com.example.demo;
 
 public class JsonResponse {
-	String status;
-	String userId;
+	String _id;
+	String password;
+	String email;
+	String username;
 
 	// getter and setter
-	public String getUserId() {
-		return userId;
+	public String get_id() {
+		return _id;
 	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
+	public void set_id(String _id) {
+		this._id = _id;
 	}
-
-	public String getStatus() {
-		return status;
+	public String getPassword() {
+		return password;
 	}
-
-	public void setStatus(String status) {
-		this.status = status;
+	public void setPassword(String password) {
+		this.password = password;
 	}
-
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
 }
